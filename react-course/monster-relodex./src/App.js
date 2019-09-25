ba34ac7bb6fb32d9713309c74f50c0cd58a49e11
luchetu <humluchetu@gmail.com>
2019-09-25 12:34:33 +0300

    hosting monsteer-roladex on github pages

---
+++ react-course/monster-relodex./src/App.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6 */
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField:''
+    };
+   
+  }
+  handleChange= (e) =>{
+    this.setState({searchField:e.target.value});
+
+  }
+ componentDidMount(){
+  
+   fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(
+     users => this.setState({monsters:users}));
+ }
+  render() {
+    const{monsters,searchField}=this.state;
+    const filterMonsters=monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+      />
+      <CardList monsters={filterMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
